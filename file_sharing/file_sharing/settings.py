d9d76176d2b60ddb350e421dfbf39aa5dacb1538
Sergio <polonium-210@mail.ru>
2025-04-29 00:02:56 +0300

    correct load_env

--- file_sharing/file_sharing/settings.py
+++ file_sharing/file_sharing/settings.py
@@ -16,7 +16,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_env = load_dotenv(BASE_DIR / '.envlocal')
+load_env = load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
